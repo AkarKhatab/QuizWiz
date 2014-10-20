9cc16ac8d53a0ca835615b72bfe17c2d6496f8ef
zolic <zolic@ed3354-03.ed3354.studat.rh6.ii.2.hb.s.cdal.chalmers.se>
2014-10-20 15:22:24 +0200

    läser från fil nu

--- src/main/java/quizwiz/io/AskQuestion.java
+++ src/main/java/quizwiz/io/AskQuestion.java
@@ -13,21 +13,23 @@ import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.bean.*;
+import javax.faces.context.FacesContext;
 
 /**
  *
  * @author zolic
  */
 @ManagedBean
-@ViewScoped
+@RequestScoped
 public class AskQuestion implements Serializable{
     
     private static final Logger LOG = Logger.getLogger(AskQuestion.class.getName());
-    private String question, ans1, ans2, ans3, ans4;
+    private String question, ans1, ans2, ans3, ans4, correctAnswer;
     private IO io;
     private ArrayList<ArrayList<String>> allQuestions;
     private ArrayList<String> nextQuestion;
     private int questNr = -1;
+    private int score = 0;
     public AskQuestion() throws UnsupportedEncodingException, FileNotFoundException, Throwable{
         io = new IO();
         getArray();
@@ -41,54 +43,67 @@ public class AskQuestion implements Serializable{
     public void getNewQuestion(){
         questNr++;
         nextQuestion = allQuestions.get(questNr);
-        LOG.log(Level.INFO, "All: {0}", nextQuestion);
         this.question = nextQuestion.get(0);
-        this.ans1 = nextQuestion.get(1);
-        this.ans2 = nextQuestion.get(2);
-        this.ans3 = nextQuestion.get(3);
-        this.ans4 = nextQuestion.get(4);
+        this.correctAnswer = nextQuestion.get(1);
+        shuffleArray(nextQuestion);
+        
     }
-
+    
+    public void shuffleArray(ArrayList<String> list){
+        ArrayList<String> shuffled = new ArrayList<>();
+        for(int i = 1; i<=4; i++){
+            shuffled.add(list.get(i));
+        }
+        Collections.shuffle(shuffled);
+        this.ans1 = shuffled.get(0);
+        this.ans2 = shuffled.get(1);
+        this.ans3 = shuffled.get(2);
+        this.ans4 = shuffled.get(3);
+        LOG.log(Level.INFO, "CorrectAnswer{0}", correctAnswer);
+    }
+    
+    public void checkIfCorrectAnswer(){
+          
+        getNewQuestion();
+    }
+    
     public String getQuestion() {
         return question;
     }
-
+    
     public void setQuestion(String question) {
         this.question = question;
     }
-
+    
     public String getAns1() {
         return ans1;
     }
-
+    
     public void setAns1(String ans1) {
         this.ans1 = ans1;
     }
-
+    
     public String getAns2() {
         return ans2;
     }
-
+    
     public void setAns2(String ans2) {
         this.ans2 = ans2;
     }
-
+    
     public String getAns3() {
         return ans3;
     }
-
+    
     public void setAns3(String ans3) {
         this.ans3 = ans3;
     }
-
+    
     public String getAns4() {
         return ans4;
     }
-
+    
     public void setAns4(String ans4) {
         this.ans4 = ans4;
     }
-    
-    
-    
 }
