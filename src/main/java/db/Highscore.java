9415c65c7638620f32f08fff8cb9e69a4fc0e993
Danijel Zolic <d.zolic21@gmail.com>
2021-03-23 09:39:48 +0100

    add recreate table file

--- src/main/java/db/Highscore.java
+++ src/main/java/db/Highscore.java
@@ -10,8 +10,6 @@ import javax.inject.Named;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
